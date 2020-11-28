fc1d2539d1a544b92708a438b885c68ebed15ff3
steeve-daniell <75067627+steeve-daniell@users.noreply.github.com>
2020-11-28 15:35:31 +0300

    Update ShibbundeAshupathri.py

--- ShibbundeAshupathri.py
+++ ShibbundeAshupathri.py
@@ -1,23 +1,33 @@
 import mysql.connector
 Password=input("ENTER PASSWORD- ")
-con=mysql.connector.connect(host="localhost",user="root",password=Password,database="shibbunde_ashupathri")
+con=mysql.connector.connect(host="localhost",user="root",password=Password)
+if con.is_connected():
+    print("connection is succesful")
 cur=con.cursor()
-cur.execute("create table if not exists Hospital_Log(Patient_Number int(4) primary key,\
-	Patient_Name varchar(20),CPR_Number int(5),Reason varchar(20),Date date)")
-       
-def insertData():
+cur.execute("create database if not exists SHMhospital")
+cur.execute("use shmhospital")
+print("database created")
+def createtable():
+    query1="create table if not exists Hospital_Log(PID int(4) not null primary key,Patient_Name varchar(20),CPR_Number int(9),Reason varchar(20),Date_of_entry date,bill decimal(6,3))"
+    cur.execute(query1)
+def insert():
     num=int(input("ENTER Patient_Number"))
     name=input("ENTER NAME")
     cpr=int(input("ENTER CPR"))
     reas=input("ENTER REASON FOR ADMISSION")
     date=input("ENTER DATE OF ADMISSION")
-    cur.execute("insert into Hospital_Log values({},'{}',{},'{}','{}')".format(num,name,cpr,reas,date))
+    bill=float(input("enter amount to be paid"))
+    cur.execute("insert into Hospital_Log values({},'{}',{},'{}','{}','{}')".format(num,name,cpr,reas,date,bill))
     con.commit()
+createtable()
+insert()
  
 def display():
     cur.execute("select * from Hospital_Log")
     for k in cur:
         print(k)
+def search():
+	
 
 while True:
 	do=int(input("DO- "))
